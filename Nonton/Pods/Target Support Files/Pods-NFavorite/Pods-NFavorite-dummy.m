323765a92c0db6e44d74256ebbe32f5131292aa2
Arya S <arya.surya021@gmail.com>
2021-01-14 10:02:10 +0700

    Deleting Local NCore on Master Branch

--- Nonton/Pods/Target Support Files/Pods-NFavorite/Pods-NFavorite-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NFavorite : NSObject
-@end
-@implementation PodsDummy_Pods_NFavorite
-@end
--- Nonton/Pods/Target Support Files/Pods-NProfile/Pods-NProfile-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NProfile : NSObject
-@end
-@implementation PodsDummy_Pods_NProfile
-@end
